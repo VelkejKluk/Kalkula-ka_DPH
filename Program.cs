13c4ca134363eae7867777f7f07942cea6da69f8
Josef Navrátil <josef.navratil@educhem.cz>
2024-01-17 18:10:19 +0100

    kokos

--- Program.cs
+++ Program.cs
@@ -8,16 +8,23 @@ if (volba != "p"&& volba != "o")
     Console.WriteLine("Chybně zadaný formát...");
     
 }
-//Zeptej se uživatele na cenu 
-Console.WriteLine("Jaká je cena produktu?");
-int cena = Convert.ToInt32(Console.ReadLine());
+else
+{
+    //Zeptej se uživatele na cenu 
+    Console.WriteLine("Jaká je cena produktu?");
+    int cena = Convert.ToInt32(Console.ReadLine());
+
+    //pdomínka, když uživatel zadá přidat nebo odebrat
+    if (volba == "p")
+    {
+        Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 121}");
+        Thread.Sleep(3000);
+    }
+    else if (volba == "o")
+    {
+        Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 79}");
+        Thread.Sleep(3000);
+    }
+}
+
     
-//pdomínka, když uživatel zadá přidat nebo odebrat
-if (volba == "p")
-{
-    Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena/100*121}");
-}
-else if(volba == "o")
-{
-    Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 79}");
-}
