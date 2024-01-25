8891fa647cd5482d90c78c5b6c3e4b29ae4a5ae7
josef.navratil <josef.navratil@VT3-08>
2024-01-25 08:15:51 +0100

    uprava pole

--- Program.cs
+++ Program.cs
@@ -1,30 +1,60 @@
 ﻿using System;
-//Chcete přidat / odebrat DPH?
-Console.WriteLine("Chcete přidat nebo odebrat DPH? [P/O]");
-string volba = Console.ReadLine().ToLower();
 
-if (volba != "p"&& volba != "o")
+while (true) 
 {
-    Console.WriteLine("Chybně zadaný formát...");
-    
+    //Chcete přidat / odebrat DPH?
+    Console.WriteLine("Chcete přidat nebo odebrat DPH? [P/O]");
+    string volba = Console.ReadLine().ToLower();
+
+    if (volba != "p"&& volba != "o")
+    {
+        Console.WriteLine("Chybně zadaný formát...");
+    }
+    else
+    {
+        Console.Write("Chcete zadat jen jednu hodnotu, nebo více? [a/n]");
+        string an  = Console.ReadLine();
+
+        if (an != "a")
+        {
+            VolbaVice();
+        }
+        else if(an != "n")
+        {
+            //Zeptej se uživatele na cenu 
+            Console.Write("Jaká je cena produktu? ");
+            int cena = Convert.ToInt32(Console.ReadLine());
+
+            //pdomínka, když uživatel zadá přidat nebo odebrat
+            if (volba == "p")
+            {
+                Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 121} Kč");
+                Console.ReadKey(); Console.Clear();
+            }
+            else if (volba == "o")
+            {
+                Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 79} Kč");
+                Console.ReadKey(); Console.Clear();
+            }
+        }
+        else
+        {
+            Console.WriteLine("chyba more");
+        }
+    }
 }
-else
+
+
+static void VolbaVice()
 {
-    //Zeptej se uživatele na cenu 
-    Console.WriteLine("Jaká je cena produktu?");
-    int cena = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Kolik hodnot chcete zadat? ");
 
-    //pdomínka, když uživatel zadá přidat nebo odebrat
-    if (volba == "p")
-    {
-        Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 121}");
-        Thread.Sleep(3000);
-    }
-    else if (volba == "o")
-    {
-        Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 79}");
-        Thread.Sleep(3000);
-    }
+    int pocet = Convert.ToInt32(Console.ReadLine());
+
+    int[] hodnoty = new int[pocet - 1];
+
+
+
+
+
 }
-
-    
